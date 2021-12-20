1c65c2914cff07f742f7b5b86fb64fc5bd1e30d5
Yadavneeraj52 <96334646+Yadavneeraj52@users.noreply.github.com>
2021-12-20 13:17:41 +0530

    Add files via upload

---
+++ info.php
@@ -0,0 +1,36 @@
+<?php
+if(isset($_POST['Title'])){
+       $POSTtitle=$_POST['Title'];
+    }
+
+$title = $_POST['Title'];
+$Firstname = $_POST['FirstName'];
+$Lastname = $_POST['LastName'];
+$Date = $_POST['birthday'];
+$Number = $_POST['Mobile'];
+$Files = $_POST['filename'];
+$Email = $_POST['email'];
+$Message = $_POST['Message'];
+
+echo "Thank You";
+echo "<br>";
+echo "Title : ".$title;
+echo "<br>";
+echo "Firstname : ".$Firstname;
+echo "<br>";
+echo "Lastname : ".$Lastname; 
+echo "<br>";
+echo "You have submitted the following details :";
+echo "<br>";
+echo "Date of Birth :  ".$Date; 
+echo "<br>";
+echo "Mobile Number :  ".$Number; 
+echo "<br>";
+echo " File Uploaded :  ".$Files; 
+echo "<br>";
+echo "Email id :" .$Email;
+echo "<br>";
+echo "Message :  ".$Message;
+echo "<br>";
+
+?>
